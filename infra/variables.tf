82869268ff4614c5d6dfedbd6ec3c861b417bdfa
Akash Sridhar <Akash.Sridhar@cdw.com>
2025-11-08 14:32:14 +0530

    Added Terraform infra for GKE cluster

---
+++ infra/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type    = string
+  default = "dash-gcp-services"
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "gke-dev-cluster"
+}
+
